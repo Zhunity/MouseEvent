788a5ac2f5fa921d1eb45308dc0c98c3d3580228
lihua <poinseting@163.com>
2020-05-28 23:45:30 +0800

    点中ui完全体

--- Assets/ClickListener.cs
+++ Assets/ClickListener.cs
@@ -3,43 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class ClickListener : MonoBehaviour
 {
-	public GraphicRaycaster ra;
+	List<RaycastResult> list = new List<RaycastResult>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
 		if(!Input.GetMouseButtonDown(0))
 		{
 			return;
 		}
-		List<RaycastResult> list = new List<RaycastResult>();
+		
 
 		//场景中的EventSystem
 
 		PointerEventData eventData = new PointerEventData(EventSystem.current);
 
 		//鼠标位置
-
-		//eventData.pressPosition = Input.mousePosition;
-
 		eventData.position = Input.mousePosition;
 
-		//Canvas的GraphicRaycaster组件，只能获取当前GraphicRaycaster下的所有UI
 
-		//并且子级中的UI组件RaycastTarget设置为true
-
-		//获取Canvas下的所有UI反馈
-
-		ra.Raycast(eventData, list);
+		EventSystem.current.RaycastAll(eventData, list);
 
 		var raycast = FindFirstRaycast(list);
 
@@ -49,13 +36,24 @@ public class ClickListener : MonoBehaviour
 			go = raycast.gameObject;
 		}
 
-		Debug.Log(go, go);
+		if(go == null)
+		{
+			Debug.Log("Click Nothing");
+		}
+		else
+		{
+			EditorGUIUtility.PingObject(go);
+			Selection.activeObject = go;
+			Debug.Log(go, go);
+		}
+		
 	}
+
 
 	/// <summary>
 	/// Return the first valid RaycastResult.
 	/// </summary>
-	protected static RaycastResult FindFirstRaycast(List<RaycastResult> candidates)
+	private RaycastResult FindFirstRaycast(List<RaycastResult> candidates)
 	{
 		for (var i = 0; i < candidates.Count; ++i)
 		{
